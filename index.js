946d8437a7830dec177cf5abd03a327675f1d851
Mohit Singh <34847423+singhm13@users.noreply.github.com>
2019-08-31 21:46:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   function readURL(input) {
     if (input.files && input.files[0]) {
       var reader = new FileReader();
-
+ 
       reader.onload = function(e) {
         $('#uploaded-image').attr('src', e.target.result);
       }
